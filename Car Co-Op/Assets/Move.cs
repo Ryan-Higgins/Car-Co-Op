31b86261acef3ba46c2b3d1423c916eb19058a09
Ryan Higgins <c16381886@mydit.ie>
2019-02-20 11:08:33 +0000

    Intial Project commit

---
+++ Car Co-Op/Assets/Move.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    private float v;
+    private float h;
+
+    public int speed = 10;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*v = Input.GetAxis("Vertical");
+        h = Input.GetAxis("Horizontal");*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        transform.position += move * speed * Time.deltaTime;
+    }
+}
