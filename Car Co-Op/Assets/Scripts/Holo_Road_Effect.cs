7ac0dfca54728ddb36f7cacf2f9706b2beaa1bad
Michael Carstairs <michael.j.carstairs@gmail.com>
2019-02-20 19:36:27 +0000

    Michael Carstairs
    - added in race Track
    - added in UFO
    -added in some materals for the track and changed the sky to black for now ( will use a sky box at a later date )

---
+++ Car Co-Op/Assets/Scripts/Holo_Road_Effect.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Holo_Road_Effect : MonoBehaviour
+{
+    Renderer rend;  
+    void Start()
+    {
+        rend = GetComponent<Renderer> ();
+        
+    }
+
+    public float offsetSpeed = 5f;
+    Vector2 textureOffset;  
+    void Update()
+    {
+        textureOffset.x += offsetSpeed * Time.deltaTime; 
+
+        rend.material.SetTextureOffset("_MainTex", textureOffset); 
+        
+    }
+}
